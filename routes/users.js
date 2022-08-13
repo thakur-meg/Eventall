6852538b06b48f40c0dc4ab634c439501e865f6a
thakur-meg <meghnathakur2001@gmail.com>
2022-08-13 22:20:33 +0530

    refractoring

--- routes/users.js
+++ routes/users.js
@@ -3,45 +3,17 @@ const express = require('express');
  const passport = require('passport');
  const catchAsync = require('../utils/catchAsync');
  const User = require('../models/user');
+ const users = require('../controllers/users');
 
- router.get('/register', (req, res) => {
-     res.render('users/register');
- });
+ router.route('/register')
+ .get(users.renderRegister)
+ .post(catchAsync(users.register));
 
- router.post('/register', catchAsync(async (req, res, next) => {
-     try {
-         const { email, username, password } = req.body;
-         const user = new User({ email, username });
-         const registeredUser = await User.register(user, password);
-         req.login(registeredUser, err => {
-             if (err) return next(err);
-             req.flash('success', 'Welcome to Eventall!');
-             res.redirect('/events');
-         })
-     } catch (e) {
-         req.flash('error', e.message);
-         res.redirect('register');
-     }
- }));
+ router.route('/login')
+     .get(users.renderLogin)
+     .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
- router.get('/login', (req, res) => {
-     res.render('users/login');
- });
 
- router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-     req.flash('success', 'Welcome Back!');
-     const redirectUrl = req.session.returnTo || '/events';
-     delete req.session.returnTo;
-     res.redirect(redirectUrl);
- });
-
- router.get('/logout', (req, res, next) => {
-    req.logout(function(err) {
-      if (err) { return next(err); }
-      req.flash('success', "Goodbye!");
-      res.redirect('/events');
-    });
-  }); 
-
+ router.get('/logout', users.logout);
 
  module.exports = router; 
