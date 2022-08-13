c8005c9f3be9fb7993ebe0250b3ca578087a7286
thakur-meg <meghnathakur2001@gmail.com>
2022-08-13 16:09:33 +0530

    breaking out routes

--- schemas.js
+++ schemas.js
@@ -4,9 +4,10 @@ const { number } = require('joi');
 module.exports.campgroundSchema = Joi.object({
      campground: Joi.object({
          title: Joi.string().required(),
-         price: Joi.number().required().min(0),
          image: Joi.string().required(),
          location: Joi.string().required(),
+         date: Joi.string().required(),
+         time: Joi.string().required(),
          description: Joi.string().required()
      }).required()
 }); 
