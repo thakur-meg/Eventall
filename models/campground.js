c054e44f1ba0963fced7222be9d31363ceaa6d1c
thakur-meg <meghnathakur2001@gmail.com>
2022-08-13 15:28:56 +0530

    review model

--- models/campground.js
+++ models/campground.js
@@ -9,6 +9,22 @@ const CampgroundSchema = new Schema({
     date : String,
     time: String,
     image: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
